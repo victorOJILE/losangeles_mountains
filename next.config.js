56cf49a0046ea5cb14462236edaddb33f36e7034
Victor Ojile <ojilevictor11@gmail.com>
2024-03-24 21:08:21 -0700

    Uncommented the exports: true in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
